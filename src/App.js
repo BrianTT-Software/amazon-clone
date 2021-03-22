1b5489db4870ce06a2f5e8f3e5535eff69fff0b7
BrianTT-Software <brian.torres15@upr.edu>
2021-03-21 23:40:53 -0400

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   // BEM naming convention
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout Component</h1>
+            {/* Checkout */}
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
