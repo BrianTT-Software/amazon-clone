e06b9455b2068493cbd20dd2a13c609a52809f9b
BrianTT-Software <brian.torres15@upr.edu>
2021-03-21 20:42:35 -0400

    initial set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  // BEM naming convention
+  return (
+    <div className="app">
+      <p>Amazon Clone</p>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
