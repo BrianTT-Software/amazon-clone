07a839f1d5ebb0d4dc7c20d98fb17faf23ef1c56
BrianTT-Software <brian.torres15@upr.edu>
2021-03-23 19:55:54 -0400

    react context api set-up

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+    basket: []
+};
+
+// always listening for action
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_TO_BASKET":
+            return {
+                ...state, // whatever the state originally was
+                basket: [...state.basket, action.item] // what ever it was plus the new item
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
