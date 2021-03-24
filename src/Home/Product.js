bcf0351313faec2ad89a305f9d83e73449d218b3
BrianTT-Software <brian.torres15@upr.edu>
2021-03-23 20:08:55 -0400

    pushes item into data layer

--- src/Home/Product.js
+++ src/Home/Product.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+    // dispatch is how we manipulate data layer
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        // dispatch and add to basket action into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -21,7 +39,7 @@ function Product({ title, image, price, rating }) {
 
             <img src={image} />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,7 @@ export const initialState = {
 
 // always listening for action
 const reducer = (state, action) => {
+    console.log(action);
     switch (action.type) {
         case "ADD_TO_BASKET":
             return {
